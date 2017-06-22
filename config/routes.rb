ff5377a71c164a346efa02871cf5141c720d0990
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-06-22 19:18:38 -0400

    login and logout pages routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :books, shallow: true
+  end
+
   devise_for :users
   root 'users#index'
 
   get '/signup', to: 'users#new'
+
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/logout', to: 'devise/sessions#destroy'
+  end
+
 end
