d0ef0cc525e7fbc96c62281a16a3200c5484b744
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-07-15 09:13:36 -0400

    finished tag sql dal

---
+++ Bookself-C#/Bookself/Bookself/DAL/ITagSource.cs
@@ -0,0 +1,16 @@
+﻿using Bookself.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookself.DAL
+{
+    public interface ITagSource
+    {
+        List<Tag> AllUserTagsForABook(int userId, int bookIsbn);
+        bool AddTag(Tag newTag);
+        bool DeleteTag(Tag tagToDelete);
+    }
+}
---
+++ Bookself-C#/Bookself/Bookself/DAL/TagSqlDal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bookself.Models;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Bookself.DAL
+{
+    public class TagSqlDal : ITagSource
+    {
+        private const string SQL_GetAllUsersTagForABook = "SELECT tag.* FROM tag INNER JOIN book_tag ON book_tag.tag_id = tag.id INNER JOIN user_book ON book_tag.book_isbn = user_book.book_isbn WHERE user_book.[user_id] = @userId AND book_tag.book_isbn = @bookIsbn ORDER BY tag.tag_name DESC;";
+        private const string SQL_DeleteTagFromBookTagTable = "DELETE FROM book_tag WHERE tag_name = @tagName;";
+        readonly string connectionString;
+
+        public TagSqlDal(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        public bool AddTag(Tag newTag)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    int rowsAffected = conn.Execute("INSERT INTO tag VALUES(tag_name = @tagName);", new { tagName = newTag.Name });
+
+                    if (rowsAffected > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Tag> AllUserTagsForABook(int userId, int bookIsbn)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    List<Tag> allUserTagsForABook = conn.Query<Tag>(SQL_GetAllUsersTagForABook, new { userId = userId, bookIsbn = bookIsbn }).ToList();
+                    return allUserTagsForABook;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteTag(Tag tagToDelete)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // deletes from book_tag table only, not tag table
+                    int rowsAffected = conn.Execute(SQL_DeleteTagFromBookTagTable, new { tagName = tagToDelete.Name });
+
+                    if (rowsAffected > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
